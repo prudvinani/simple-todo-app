0580acbf54564c74f2c0e6bf9086ee08868fe1da
usersfro <palvaiuser@gmail.com>
2024-12-02 00:32:24 +0530

    init

--- server/src/index.ts
+++ server/src/index.ts
@@ -23,11 +23,9 @@ const app = express();
 
 
 app.use(express.json());
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(cors({ credentials: true }));
 
-// Routes
 
-// Test route
 app.get("/", (req: Request, res: Response) => {
   res.status(200).send("Welcome to the Todo App API!");
 });
